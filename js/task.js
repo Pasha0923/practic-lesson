f5265400a44988d6d0f5df19b5590c2332441a69
Pasha0923 <alexsai@i.ua>
2023-11-23 17:50:05 +0300

    add result

--- js/task.js
+++ js/task.js
@@ -1,1 +1,220 @@
 "use strict";
+// ЗАДАЧА №4
+// Напиши стрілочну функцію getTotalBalanceByGender(users, gender), яка прийматиме два параметра:
+// перший параметр users — масив об’єктів користувачів,
+// другий параметр gender — рядок, що зберігає стать.
+// Функція має використовувати ланцюжок виклику методів
+// та повертати загальний баланс користувачів(властивість balance),
+// стать яких(властивість gender) збігається зі значенням параметра gender.
+// const allUsers = [
+//   {
+//     name: "Moore Hensley",
+//     gender: "male",
+//     balance: 2811,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     gender: "female",
+//     balance: 3821,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     gender: "male",
+//     balance: 3793,
+//   },
+//   {
+//     name: "Elma Head",
+//     gender: "female",
+//     balance: 2278,
+//   },
+//   {
+//     name: "Carey Barr",
+//     gender: "male",
+//     balance: 3951,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     gender: "male",
+//     balance: 1498,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     gender: "female",
+//     balance: 2764,
+//   },
+// ];
+
+// const getTotalBalanceByGender = (users, gender) => {
+//   return users
+//     .filter((user) => user.gender === gender)
+//     .reduce((sum, bal) => sum + bal.balance, 0);
+// };
+// console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+// console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+
+// const names = books
+//   .filter((book) => book.rating >= MIN_BOOK_RATING)
+//   .map((book) => book.author)
+//   .toSorted((firstAuthor, secondAuthor) =>
+//     firstAuthor.localeCompare(secondAuthor)
+//   );
+
+// ЗАДАЧА № 3
+// .Напиши стрілочну функцію sortByDescendingFriendCount(users), яка прийматиме один параметр users — масив об’єктів користувачів.
+// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів
+// (властивість friends).
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її роботи.
+
+// const sortByDescendingFriendCount = (users) => {
+//   return users.toSorted((current, next) => next.friends - current.friends);
+// };
+// console.log(
+//   sortByDescendingFriendCount([
+//     {
+//       name: "Moore Hensley",
+//       friends: ["Sharron Pace"],
+//       gender: "male",
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       gender: "female",
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       gender: "male",
+//     },
+//     {
+//       name: "Elma Head",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       gender: "female",
+//     },
+//     {
+//       name: "Carey Barr",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       gender: "male",
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       gender: "male",
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       gender: "female",
+//     },
+//   ])
+// );
+
+// ЗАДАЧА №2
+
+// const allUsers = [
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//   },
+// ];
+// ЗАДАЧА №2
+// Напиши стрілочну функцію getUsersWithFriend(users, friendName) , яка прийматиме два параметра:
+// перший параметр users — масив об’єктів користувачів
+// другий параметр friendName — ім’я друга для пошуку.
+// Функція має повертати масив усіх користувачів із масиву users, у яких є друг з іменем friendName.
+// Друзі кожного користувача зберігаються у властивості friends.Якщо користувачів,
+//  у яких є такий других немає, то функція має повернути порожній масив.
+
+// ПОРАДИ
+// 1)  Метод filter() можна використовувати для створення нового масиву з елементами, які задовольняють певну умову.
+// 2) Використовуй метод includes() для перевірки, чи масив friends містить friendName.
+// const getUsersWithFriend = (users, friendName) => {
+//   return users.filter((user) => user.friends.includes(friendName));
+// };
+// console.log(getUsersWithFriend(allUsers, "Briana Decker"));
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+// ЗАДАЧА №1
+// Задача 1. Імена користувачів
+// Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об’єктів користувачів.
+//  Функція має повертати масив імен усіх користувачів(властивість name) із масиву users.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її викликів.
+
+const getUserNames = (users) => {
+  return users.map((user) => user.name);
+};
+console.log(
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811,
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821,
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793,
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278,
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951,
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498,
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764,
+    },
+  ])
+);
